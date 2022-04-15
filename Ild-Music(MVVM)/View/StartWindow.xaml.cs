9962a90390b459823a8053f40c65a8e763b227d5
ggghosthat <ildarildar990@gmail.com>
2022-04-15 06:23:16 +0300

    Go 2 MVVM - tested [2]

--- Ild-Music(MVVM)/View/StartWindow.xaml.cs
+++ Ild-Music(MVVM)/View/StartWindow.xaml.cs
@@ -1,52 +1,16 @@
-﻿
-using SynchronizationBlock.Models.SynchArea;
-using System;
+﻿using Ild_Music_MVVM_.ViewModel.VM;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Linq;
-using System.Collections.Generic;
-using Ild_Music_CORE.Models.Core.Tracklist_Structure;
-using Ild_Music_CORE.Models;
 
-namespace Ild_Music.UI
+namespace Ild_Music_MVVM_.View
 {
     public partial class StartWindow : Window
     {
-    //    #region PlayerFields
-    //    private PlayerController _playerController;
-    //    IPlayerControll PlayerController => _playerController ?? null;
-
-    //    private int initIndex = 0;
-    //    private Tracklist inputTracklist;
-
-    //    private ITrackable _playerStaffTrack;
-    //    private ITrackable _playerStaffTracklist;
-
-    //    private event Action DropStaff;
-    //    #endregion
-
-        #region SynchBlockFields
-        private static Area _mainArea;
-        //private static ModelFactoryController _modelFactory;
-        //private static WindowsController _windowsController;
-        //private static SynchSupporter _supporter;
-
-        private IDictionary<string,string> _tracksStructure = new Dictionary<string, string>();
-        private IDictionary<string, string> _playlistsStructure = new Dictionary<string, string>();
-        private IDictionary<string, string> _artistsStructure = new Dictionary<string, string>();
-
-        
-        internal delegate void RefreshCenteralList();
-        internal static event RefreshCenteralList RefreshCentList;
-        #endregion
-
-
 
         #region Ctors
         public StartWindow()
         {
+            DataContext = new StartViewModel();
         }
         #endregion
 
@@ -77,92 +41,92 @@ namespace Ild_Music.UI
 
         private void EnterMouse_Artists(object sender, MouseEventArgs e)
         {
-            Artists.Background = new SolidColorBrush(Color.FromRgb(25, 31, 158));
+           //Artists.Background = new SolidColorBrush(Color.FromRgb(25, 31, 158));
         }
 
         private void LeaveMouse_Artists(object sender, MouseEventArgs e)
         {
-            Artists.Background = new SolidColorBrush(Color.FromRgb(31, 38, 178));
+            //Artists.Background = new SolidColorBrush(Color.FromRgb(31, 38, 178));
         }
 
         private void EnterMouse_Home(object sender, MouseEventArgs e)
         {
-            Home.Background = new SolidColorBrush(Color.FromRgb(25, 31, 158));
+            //Home.Background = new SolidColorBrush(Color.FromRgb(25, 31, 158));
         }
 
         private void LeaverMouse_Home(object sender, MouseEventArgs e)
         {
-            Home.Background = new SolidColorBrush(Color.FromRgb(31, 38, 178));
+            //Home.Background = new SolidColorBrush(Color.FromRgb(31, 38, 178));
         }
 
         private void EnterMouse_Playlists(object sender, MouseEventArgs e)
         {
-            Playlists.Background = new SolidColorBrush(Color.FromRgb(25, 31, 158));
+            //Playlists.Background = new SolidColorBrush(Color.FromRgb(25, 31, 158));
         }
 
         private void LeaveMouse_Playlists(object sender, MouseEventArgs e)
         {
-            Playlists.Background = new SolidColorBrush(Color.FromRgb(31, 38, 178));
+            //Playlists.Background = new SolidColorBrush(Color.FromRgb(31, 38, 178));
         }
 
         private void EnterMouse_Tracks(object sender, MouseEventArgs e)
         {
-            Tracks.Background = new SolidColorBrush(Color.FromRgb(25, 31, 158));
+            //Tracks.Background = new SolidColorBrush(Color.FromRgb(25, 31, 158));
         }
 
         private void LeaveMouse_Tracks(object sender, MouseEventArgs e)
         {
-            Tracks.Background = new SolidColorBrush(Color.FromRgb(31, 38, 178));
+            //Tracks.Background = new SolidColorBrush(Color.FromRgb(31, 38, 178));
         }
 
         private void Previous_MouseEnter(object sender, MouseEventArgs e)
         {
-            brPrev.Background = new SolidColorBrush(Colors.Aqua);
+            //brPrev.Background = new SolidColorBrush(Colors.Aqua);
         }
 
         private void Previous_MouseLeave(object sender, MouseEventArgs e)
         {
-            brPrev.Background = new SolidColorBrush(Colors.Transparent);
+            //brPrev.Background = new SolidColorBrush(Colors.Transparent);
         }
 
         private void Play_MouseEnter(object sender, MouseEventArgs e)
         {
-            brPlay.Background = new SolidColorBrush(Colors.Aqua);
+            //brPlay.Background = new SolidColorBrush(Colors.Aqua);
         }
 
         private void Play_MouseLeave(object sender, MouseEventArgs e)
         {
-            brPlay.Background = new SolidColorBrush(Colors.Transparent);
+            //brPlay.Background = new SolidColorBrush(Colors.Transparent);
         }
 
         private void Next_MouseEnter(object sender, MouseEventArgs e)
         {
-            brNext.Background = new SolidColorBrush(Colors.Aqua);
+            //brNext.Background = new SolidColorBrush(Colors.Aqua);
         }
 
         private void Next_MouseLeave(object sender, MouseEventArgs e)
         {
-            brNext.Background = new SolidColorBrush(Colors.Transparent);
+            //brNext.Background = new SolidColorBrush(Colors.Transparent);
         }
 
         private void Shuffle_MouseEnter(object sender, MouseEventArgs e)
         {
-            brShuffle.Background = new SolidColorBrush(Colors.Aqua);
+            //brShuffle.Background = new SolidColorBrush(Colors.Aqua);
         }
 
         private void Shuffle_MouseLeave(object sender, MouseEventArgs e)
         {
-            brShuffle.Background = new SolidColorBrush(Colors.Transparent);
+            //brShuffle.Background = new SolidColorBrush(Colors.Transparent);
         }
 
         private void Repeat_MouseEnter(object sender, MouseEventArgs e)
         {
-            brRepeat.Background = new SolidColorBrush(Colors.Aqua);
+            //brRepeat.Background = new SolidColorBrush(Colors.Aqua);
         }
 
         private void Repeat_MouseLeave(object sender, MouseEventArgs e)
         {
-            brRepeat.Background = new SolidColorBrush(Colors.Transparent);
+            //brRepeat.Background = new SolidColorBrush(Colors.Transparent);
         }
 
      
@@ -287,10 +251,5 @@ namespace Ild_Music.UI
             //    EnablePlayer(PlayerTrigger.REPEAT);
         }
         #endregion
-
-        private void OnDropStaff() 
-        {
-            //_playerController.DropStaff(_playerStaffTracklist);
-        }
     }
 }
