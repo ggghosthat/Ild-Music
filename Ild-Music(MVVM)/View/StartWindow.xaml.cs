cd0c92bda22bbaef72bdb9d72468da8a19a180c8
ggghosthat <ildarildar990@gmail.com>
2022-07-29 09:21:03 +0300

    minimize and close button added

--- Ild-Music(MVVM)/View/StartWindow.xaml.cs
+++ Ild-Music(MVVM)/View/StartWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Ild_Music_MVVM_.Services;
+﻿using System.Windows;
 using Ild_Music_MVVM_.ViewModel.VM;
 
 namespace Ild_Music_MVVM_.View
@@ -26,5 +26,16 @@ namespace Ild_Music_MVVM_.View
 
         private void PlatformSliderClick(object sender, System.Windows.Input.MouseButtonEventArgs e) =>
             mainViewModel.SetVM(new StageViewModel());
+
+
+
+
+
+        private void btnMinimizeWindow_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e) =>
+            WindowState = WindowState.Minimized;
+        
+
+        private void btnCloseWindow_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e) =>
+            Application.Current.Shutdown();
     }
 }
