aa054c6b4a54c141e477395301f74421598108e6
ggghosthat <ildarildar990@gmail.com>
2023-03-28 00:19:28 +0300

    StartView lists icons fixed by moving to assets' pngs

--- Ild-Music/Converters/InstanceConverter.cs
+++ Ild-Music/Converters/InstanceConverter.cs
@@ -3,6 +3,7 @@ using ShareInstances.Instances.Interfaces;
 
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Platform;
 using Avalonia.Media.Imaging;
 using Avalonia.Data.Converters;
 using System;
@@ -28,24 +29,7 @@ public class InstanceConverter : IValueConverter
         }
         else if (parameter == "ico_def")
         {
-            if (value is ICoreEntity entity)
-            {
-                if (entity.AvatarBase64 != null)
-                {
-                    return entity.GetAvatar();
-                }
-                else
-                {
-                    if (entity is Artist artist)
-                        return Application.Current.FindResource("DefaultArtistIcon");
-                    else if (entity is Playlist playlist)
-                        return Application.Current.FindResource("DefaultPlaylistIcon");
-                    else if (entity is Track track)
-                        return Application.Current.FindResource("DefaultTrackIcon");        
-                    else return null;        
-                }
-            }
-            else return null;
+            return null;
         }
         else if (parameter == "ico_col")
         {
@@ -53,16 +37,16 @@ public class InstanceConverter : IValueConverter
             {
                 if (entity.AvatarBase64 != null)
                 {
-                    return entity.GetAvatar();
+                    return CraftImage(ref entity);
                 }
                 else
                 {
                     if (value is Artist artist)
-                        return Application.Current.FindResource("ColoredArtistIcon");
+                        return LoadAsset(@"avares://Ild_Music/Assets/DefaultIcons/artist.png");
                     else if (value is Playlist playlist)
-                        return Application.Current.FindResource("ColoredPlaylistIcon");
+                        return LoadAsset(@"avares://Ild_Music/Assets/DefaultIcons/playlist.png");
                     else if (value is Track track)
-                        return Application.Current.FindResource("ColoredTrackIcon");
+                        return LoadAsset(@"avares://Ild_Music/Assets/DefaultIcons/track.png");
                     else return null;
                 }
             }
@@ -120,5 +104,22 @@ public class InstanceConverter : IValueConverter
 
         image.Source = new Bitmap(new MemoryStream(source));
         return resource;
+    }
+
+    private object CraftImage(ref ICoreEntity entity)
+    {
+        var image = new Image();
+        image.Source = new Bitmap(new MemoryStream( entity.GetAvatar() ));
+        return image;
+    }
+
+    private object LoadAsset(string path)
+    {
+        var assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
+        var bitmap = new Bitmap(assets.Open(new Uri(path)));
+
+        var image = new Image();
+        image.Source = bitmap;
+        return image;
     }
 }
