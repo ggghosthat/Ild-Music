f47c8ff4102284a4fcb26aeb91eb14c7664db682
ggghosthat <ildarildar990@gmail.com>
2024-04-30 10:31:05 +0300

    add searching

--- Ild-Music.Cube/Guido/Agents/InitAgent.cs
+++ Ild-Music.Cube/Guido/Agents/InitAgent.cs
@@ -59,20 +59,20 @@ internal static class ConnectionAgent
                 connection.Execute("create table if not exists tags_instances(Id integer primary key, TagID, IID, EntityType integer)");
 
                 //create indexing virtual tables with FTS5 sqlite's extenssion
-                connection.Execute("create virtual table if not exists artists_index using fts5 (Name, Description, Year)");
-                connection.Execute("create virtual table if not exists playlists_index using fts5 (Name, Description, Year)");
-                connection.Execute("create virtual table if not exists tracks_index using fts5 (Name, Description, Year)");
-                connection.Execute("create virtual table if not exists tags_index using fts5 (Name)");
+                connection.Execute("create virtual table if not exists artists_index using fts5 (AID, Name, Description, Year)");
+                connection.Execute("create virtual table if not exists playlists_index using fts5 (PID, Name, Description, Year)");
+                connection.Execute("create virtual table if not exists tracks_index using fts5 (TID, Name, Description, Year)");
+                connection.Execute("create virtual table if not exists tags_index using fts5 (TagId, Name)");
            
                 //insert trigger for virtual tables
-                connection.Execute("create trigger if not exists insert_artist after insert on artists begin insert into artists_index (Name, Description, Year) values(NEW.Name, New.Description, New.Year); end;");
-                connection.Execute("create trigger if not exists insert_playlist after insert on playlists begin insert into playlists_index (Name, Description, Year) values(NEW.Name, New.Description, New.Year); end;");
-                connection.Execute("create trigger if not exists insert_track after insert on tracks begin insert into tracks_index (Name, Description, Year) values(NEW.Name, New.Description, New.Year); end;");
+                connection.Execute("create trigger if not exists insert_artist after insert on artists begin insert into artists_index (AID, Name, Description, Year) values(NEW.AID, NEW.Name, New.Description, New.Year); end;");
+                connection.Execute("create trigger if not exists insert_playlist after insert on playlists begin insert into playlists_index (PID, Name, Description, Year) values(NEW.PID, NEW.Name, New.Description, New.Year); end;");
+                connection.Execute("create trigger if not exists insert_track after insert on tracks begin insert into tracks_index (TID, Name, Description, Year) values(NEW.TID, NEW.Name, New.Description, New.Year); end;");
 
                 //update triggers for virtual tables
-                connection.Execute("create trigger if not exists update_artist after update on artists begin update artists_index set Name=New.Name, Description=New.Description, Year=New.Year where AID=New.AID; end;");
-                connection.Execute("create trigger if not exists update_playlist after update on playlists begin update playlists_index set Name=New.Name, Description=New.Description, Year=New.Year where PID=New.PID; end;");
-                connection.Execute("create trigger if not exists update_track after update on tracks  begin update tracks_index set Name=New.Name, Description=New.Description, Year=New.Year where TID=New.TID; end;");
+                connection.Execute("create trigger if not exists update_artist after update on artists begin update artists_index set AID=NEW.AID, Name=NEW.Name, Description=NEW.Description, Year=NEW.Year where AID=NEW.AID; end;");
+                connection.Execute("create trigger if not exists update_playlist after update on playlists begin update playlists_index set PID=NEW.PID, Name=NEW.Name, Description=NEW.Description, Year=NEW.Year where PID=NEW.PID; end;");
+                connection.Execute("create trigger if not exists update_track after update on tracks  begin update tracks_index set TID=NEW.TID, Name=NEW.Name, Description=NEW.Description, Year=NEW.Year where TID=NEW.TID; end;");
             }
         }
         catch(Exception ex)
--- Ild-Music.Cube/Guido/Handlers/SearchHandler.cs
+++ Ild-Music.Cube/Guido/Handlers/SearchHandler.cs
@@ -1,4 +1,10 @@
 using Ild_Music.Core.Instances.DTO;
+using Ild_Music.Core.Instances.Querying;
+using Cube.Guido.Agents;
+using Ild_Music.Core.Instances;
+
+using System.Data;
+using Dapper;
 
 namespace Cube.Guido.Handlers;
 
@@ -8,8 +14,229 @@ internal sealed class SearchHandler
     {
     }
 
-    public Task<IEnumerable<CommonInstanceDTO>> Search()
+    public Task<IEnumerable<CommonInstanceDTO>> Search(string searchQuery)
     {
-       return null; 
+        List<CommonInstanceDTO> resultCollection = new ();
+        using (IDbConnection connection = ConnectionAgent.GetDbConnection())
+        {
+            connection.Open();
+
+            using (var transaction = connection.BeginTransaction())
+            {
+                IEnumerable<string> artistsId = null;
+                IEnumerable<string> playlistsId = null;
+                IEnumerable<string> tracksId = null;
+                IEnumerable<string> tagsId = null;
+
+                var commonSearchQuery = @"
+                    SELECT AID FROM artists_index MATCH @query;
+                    SELECT PID FROM playlists_index MATCH @query;
+                    SELECT TID FROM tracks_index MATCH @query;
+                    SELECT TagId FROM tags_index MATCH @query;";
+                
+                var commonQuery = @"
+                    SELECT AID, Name, Avatar FROM artists
+                    WHERE AID IN @aids;
+
+			        SELECT PID, Name, Avatar FROM playlists
+			        WHERE PID IN @pids;			
+			    
+                    SELECT TID, Name, Avatar FROM tracks
+			        WHERE TID IN @tids;                                    
+                    
+                    SELECT TagID, Name, Color FROM tags
+                    WHERE TagID IN @tagids;";
+
+                using (var multiple = connection
+                           .QueryMultiple(commonSearchQuery,
+                           new {query=searchQuery}))
+                {
+                    artistsId = multiple.Read<string>();
+                    playlistsId = multiple.Read<string>();
+                    tracksId = multiple.Read<string>();
+                    tagsId = multiple.Read<string>();
+                }
+                               
+                using(var multiQuery = connection
+                          .QueryMultiple(commonQuery, 
+                          new {aids = artistsId,
+                               pids = playlistsId,
+                               tids = tracksId,
+                               tagids = tagsId}))
+                {
+                    var artistsDTO = multiQuery.Read()
+                        .Select(a => new CommonInstanceDTO( 
+                            id: new Guid(a.AID),
+                            name: ((string)a.Name).AsMemory(),
+                            avatar: a.Avatar,
+                            tag: EntityTag.ARTIST))
+                        .ToList();
+
+                    resultCollection.AddRange(artistsDTO); 
+
+                    var playlistsDTO = multiQuery.Read()
+                        .Select(p => new CommonInstanceDTO( 
+                            id: new Guid(p.PID),
+                            name: ((string)p.Name).AsMemory(),
+                            avatar: p.Avatar,
+                            tag: EntityTag.PLAYLIST))
+                        .ToList(); 
+
+                    resultCollection.AddRange(playlistsDTO);
+
+                    var tracksDTO = multiQuery.Read()
+                        .Select(t => new CommonInstanceDTO( 
+                            id: new Guid(t.TID),
+                            name: ((string)t.Name).AsMemory(),
+                            avatar: t.Avatar,
+                            tag: EntityTag.TRACK))
+                        .ToList();
+
+                    resultCollection.AddRange(tracksDTO);
+                }
+            }
+        }
+
+        return Task.FromResult<IEnumerable<CommonInstanceDTO>>(resultCollection);
+    }
+
+    public Task<IEnumerable<CommonInstanceDTO>> SearchArtists(string searchQuery)
+    {
+        List<CommonInstanceDTO> resultCollection = new ();
+        using (IDbConnection connection = ConnectionAgent.GetDbConnection())
+        {
+            connection.Open();
+
+            using (var transaction = connection.BeginTransaction())
+            {
+                var commonSearchQuery = @"SELECT AID FROM artists_index MATCH @query;";                
+                var commonQuery = @"SELECT AID, Name, Avatar FROM artists WHERE AID IN @aids;";
+
+                var artistsId = connection.Query<string>(
+                    commonSearchQuery,
+                    new {query=searchQuery},
+                    transaction);
+                               
+                var artistsDTO = connection.Query(
+                    commonQuery,
+                    new {aids = artistsId},
+                    transaction)
+                .Select(a => new CommonInstanceDTO( 
+                    id: new Guid(a.AID),
+                    name: ((string)a.Name).AsMemory(),
+                    avatar: a.Avatar,
+                    tag: EntityTag.ARTIST)).ToList();
+
+                resultCollection.AddRange(artistsDTO);  
+            }
+        }
+
+        return Task.FromResult<IEnumerable<CommonInstanceDTO>>(resultCollection);
+    }
+
+    public Task<IEnumerable<CommonInstanceDTO>> SearchPlaylists(string searchQuery)
+    {
+        List<CommonInstanceDTO> resultCollection = new ();
+        using (IDbConnection connection = ConnectionAgent.GetDbConnection())
+        {
+            connection.Open();
+
+            using (var transaction = connection.BeginTransaction())
+            {
+
+                var commonSearchQuery = @"SELECT PID FROM playlists_index MATCH @query;";                
+                var commonQuery = @"SELECT PID, Name, Avatar FROM playlists	WHERE PID IN @pids;	";
+
+                var playlistsId = connection.Query<string>(
+                    commonSearchQuery,
+                    new {query=searchQuery},
+                    transaction);
+                               
+                var playlistsDTO = connection.Query(
+                    commonQuery,
+                    new {pids = playlistsId},
+                    transaction)                
+                .Select(p => new CommonInstanceDTO( 
+                    id: new Guid(p.PID),
+                    name: ((string)p.Name).AsMemory(),
+                    avatar: p.Avatar,
+                    tag: EntityTag.PLAYLIST))
+                .ToList(); 
+
+                resultCollection.AddRange(playlistsDTO); 
+            }
+        }
+
+        return Task.FromResult<IEnumerable<CommonInstanceDTO>>(resultCollection);
+    }
+
+    public Task<IEnumerable<CommonInstanceDTO>> SearchTracks(string searchQuery)
+    {
+        List<CommonInstanceDTO> resultCollection = new ();
+        using (IDbConnection connection = ConnectionAgent.GetDbConnection())
+        {
+            connection.Open();
+
+            using (var transaction = connection.BeginTransaction())
+            {
+
+                var commonSearchQuery = @"SELECT TID FROM tracks_index MATCH @query;";                
+                var commonQuery = @"SELECT TID, Name, Avatar FROM tracks WHERE TID IN @tids;";
+
+                var tracksId = connection.Query<string>(
+                    commonSearchQuery,
+                    new {query=searchQuery},
+                    transaction);
+                               
+                var tracksDTO = connection.Query(
+                    commonQuery,
+                    new {tids = tracksId},
+                    transaction) 
+                .Select(t => new CommonInstanceDTO( 
+                    id: new Guid(t.TID),
+                    name: ((string)t.Name).AsMemory(),
+                    avatar: t.Avatar,
+                    tag: EntityTag.TRACK))
+                .ToList();
+
+                resultCollection.AddRange(tracksDTO);                
+            }
+        }
+
+        return Task.FromResult<IEnumerable<CommonInstanceDTO>>(resultCollection);
+    }
+
+    public Task<IEnumerable<Tag>> SearchTags(string searchQuery)
+    {
+        List<Tag> resultCollection = new ();
+        using (IDbConnection connection = ConnectionAgent.GetDbConnection())
+        {
+            connection.Open();
+
+            using (var transaction = connection.BeginTransaction())
+            {
+                var commonSearchQuery = @"SELECT AID FROM artists_index MATCH @query;";                
+                var commonQuery = @"SELECT TagID, Name, Color FROM tags WHERE TagID IN @tagids;";
+
+                var tagsId = connection.Query<string>(
+                    commonSearchQuery,
+                    new {query=searchQuery},
+                    transaction);
+                               
+                var tagsDTO = connection.Query(
+                    commonQuery,
+                    new {tagids = tagsId},
+                    transaction)
+                .Select(tag => new Tag(
+                    id: new Guid(tag.Id),
+                    name: ((string)tag.Name).AsMemory(),
+                    color: ((string)tag.Color).AsMemory()))
+                .ToList();
+
+                resultCollection.AddRange(tagsDTO); 
+            }
+        }
+
+        return Task.FromResult<IEnumerable<Tag>>(resultCollection);
     }
 }
