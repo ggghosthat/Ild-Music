438dbc170c9f954e9531448fc9b3b9da099aeda1
gghosthat <ildarildar990@gmail.com>
2024-09-23 21:09:51 +0300

    update build scripts directory

--- build/linux/publish-linux.sh
+++ build/linux/publish-linux.sh
@@ -5,16 +5,16 @@ out_header="Ild-Music.linux"
 
 #prepare allocation directory
 function allocate_out_dir () {
-    mkdir "../out/"
-    mkdir "../out/$out_header/";
-    mkdir "../out/$out_header/components/";
+    mkdir "../../out/"
+    mkdir "../../out/$out_header/";
+    mkdir "../../out/$out_header/components/";
 }
 
 #publish component instance
 function process_component () {
     local component=$1
     local component_path=$2
-    local component_out_directory="../out/$out_header/components/$component/"
+    local component_out_directory="../../out/$out_header/components/$component/"
 
     mkdir $component_out_directory
     dotnet publish $component_path -c Release --framework=net8.0 --runtime=linux-x64 --self-contained -o=$component_out_directory
@@ -23,7 +23,7 @@ function process_component () {
 #publish app instance
 function process_app() {
     local app_path=$1
-    local app_out_directory="../out/$out_header"
+    local app_out_directory="../../out/$out_header"
 
     dotnet publish $app_path -c Release --framework=net8.0 --runtime=linux-x64 --self-contained -p PublishSingleFile=True -o=$app_out_directory
     
@@ -38,16 +38,16 @@ function main () {
 
     #publish 'Repository' component 
     local comp1="Ild-Music.Repository"
-    local comp_path1="../src/Ild-Music.Repository/Ild-Music.Repository.csproj"
+    local comp_path1="../../src/Ild-Music.Repository/Ild-Music.Repository.csproj"
     process_component $comp1 $comp_path1;
     
     #publish 'LinuxPlayer' component
     local comp2="Ild-Music.VlcPlayer.Linux"
-    local comp_path2="../src/Ild-Music.VlcPlayer.Linux/Ild-Music.VlcPlayer.Linux.csproj"
+    local comp_path2="../../src/Ild-Music.VlcPlayer.Linux/Ild-Music.VlcPlayer.Linux.csproj"
     process_component $comp2 $comp_path2;
 
     #publish 'app' component
-    local app="../src/Ild-Music/Ild-Music.csproj"
+    local app="../../src/Ild-Music/Ild-Music.csproj"
     process_app $app;
 }
 
