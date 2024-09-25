5f44dd0debef771e0732794546c53e9cc8813338
ggghosthat <ildarildar990@gmail.com>
2024-09-25 11:47:05 +0300

    update readme image, build script

--- build/windows/build_app_dev_env.ps1
+++ build/windows/build_app_dev_env.ps1
@@ -1,5 +1,5 @@
-$comp1="../../src/Ild-Music.Repository/"
+$comp1="../../src/Ild-Music.Repository/Ild-Music.Repository.csproj"
 & "dotnet" publish $comp1 -c Release --framework=net8.0 --runtime=linux-x64 --self-contained
 
-$comp2="../../src/Ild-Music.NAudio.Windows/"
+$comp2="../../src/Ild-Music.NAudio.Windows/Ild-Music.NAudio.Windows.csproj"
 & "dotnet" publish $comp2 -c Release --framework=net8.0 --runtime=linux-x64 --self-contained
